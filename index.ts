3c9866aba925b8958df65ff8fd6034fc89447320
Benjamin Efigenio <befigenio@gmail.com>
2022-11-16 14:01:11 +0200

    update event

--- index.ts
+++ index.ts
@@ -1,12 +1,8 @@
 import { EventBridgeEvent } from "aws-lambda";
-
-import { AccountPortfolioBalance, PositionPnl } from "@lyrafinance/lyra-js";
-import { fromBigNumber } from "./src/utils/format";
-import { lyra } from './src/lyra';
 import { vaults } from './src/query/vaults';
 
-// export const handler = async (event: EventBridgeEvent<"otus-service-cron", any>) => {
-export const handler = async () => {
+export const handler = async (event: EventBridgeEvent<"otus-service-cron", any>) => {
+  // export const handler = async () => {
   // get supported asset prices
   // get active vaults from subgraph (active, has hedge strategy, has trades)
   // get position information from lyra
@@ -18,12 +14,6 @@ export const handler = async () => {
 
   console.log({ activeVaults })
 
-  await activeVaults.map(async vault => {
-    const { vaultTrades } = vault;
-    const { positionId } = vault;
-    const position = positionId;
-  })
-
 }
 
-handler(); 
+// handler(); 
